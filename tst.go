97bec74cdd14a434cbde7e562a8aa48a2135cd24
Andres Ruiz <test@example.com>
2024-04-19 11:32:00 -0500

    Remove OK as it's unused

--- tst.go
+++ tst.go
@@ -2,15 +2,6 @@ package andres_ruiz_golang
 
 import "testing"
 
-// Ok calls t.Fatal(err) If err is not nil
-func Ok(t *testing.T, err error) {
-	t.Helper()
-	if err != nil {
-		t.Fatal(err)
-
-	}
-}
-
 // True calls t.Error() If expr is false
 func True(t *testing.T, expr bool, fmtMsg string, vals ...interface{}) {
 	t.Helper()
